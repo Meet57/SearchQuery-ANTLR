e3a8f82de73df11c039c4ceb36ad2b2d0e203d11
meet57 <meet57.mp@gmail.com>
2022-05-26 15:32:09 +0530

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Vue 2
+## BoilerPlate with ant-design, tailwind, vueX, vue-router, PWA
+
+ant-design : https://1x.antdv.com/
+
+tailwind css : https://tailwindcss.com/
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from "vue";
+import App from "./App.vue";
+import "./index.css";
+import "./registerServiceWorker";
+import "ant-design-vue/dist/antd.css";
+import Antd from "ant-design-vue";
+
+Vue.config.productionTip = false;
+Vue.use(Antd);
+
+new Vue({
+  router,
+  store,
+  render: function (h) {
+    return h(App);
+  },
+}).$mount("#app");
