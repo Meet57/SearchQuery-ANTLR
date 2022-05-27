3907d1c62f50dc8c7699b221e810bbee6dff6386
meet57 <meet57.mp@gmail.com>
2022-05-27 12:09:48 +0530

    codemirror added

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -6,8 +6,14 @@
       </a-col>
     </a-row>
     <a-row>
-      <a-col :span="12">
-        <a-input placeholder="Basic usage" v-model="query" />
+      <a-col :span="20">
+        <codemirror
+          id="CodeMirror"
+          :options="options"
+          placeholder="Basic usage"
+          @ready="onEditorReady"
+          v-model="query"
+        />
       </a-col>
     </a-row>
     <a-row class="mt-10">
@@ -20,19 +26,43 @@
 
 <script>
 import OutputTable from "./OutputTable.vue";
+import { codemirror } from "vue-codemirror";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/dracula.css";
 
 export default {
   name: "SearchBar",
   components: {
     OutputTable,
+    codemirror,
   },
   data() {
     return {
       query:
         'show top 10 sourceip:"192.168.0.1" metric:"system.network.interface~in.bytes.per.sec"',
+      options: {
+        theme: "dracula",
+      },
     };
+  },
+  methods: {
+    onEditorReady(editor) {
+      editor.focus();
+      editor.on("beforeChange", this.cleanNewLines);
+    },
+    cleanNewLines(instance, change) {
+      var newtext = change.text.join("").replace(/\n/g, ""); // remove ALL \n !
+      change.update(change.from, change.to, [newtext]);
+      return true;
+    },
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+#CodeMirror {
+  border: 1px solid #eee;
+  height: 40px !important;
+  overflow-y: hidden;
+}
+</style>
